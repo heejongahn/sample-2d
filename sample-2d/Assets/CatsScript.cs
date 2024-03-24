4e2e7854e734e48b458bc152a067dcc27b389af5
Ahn Heejong <heejongahn@gmail.com>
2024-03-24 11:07:03 +0900

    Add basic objects

---
+++ sample-2d/Assets/CatsScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatsScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float acc = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myRigidBody.velocity += Vector2.up * acc;
+        }
+    }
+}
